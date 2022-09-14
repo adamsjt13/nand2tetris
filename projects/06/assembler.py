1d8607d3f9821603242bf5260f7bb092ba53b0ec
Jonathan Adams <adamsjt13@gmail.com>
2022-09-13 20:32:30 -0500

    Implementing Code module. Updating assembly.py to test new Code module

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,9 +1,9 @@
 import parser
-
+import code
 
 def main(args):
     file_parser = parser.Parser(args)
-
+    code_builder = code.Code()
     keep_going = True
     
     while keep_going:
@@ -16,9 +16,9 @@ def main(args):
         if instruction == 'A_INSTRUCTION' or instruction == 'L_INSTRUCTION':
             print('Symbol: ', file_parser.symbol())
         else:
-            print('Dest: ', file_parser.dest())
-            print('Comp: ', file_parser.comp())
-            print('Jump: ', file_parser.jump())
+            print('Dest: ', file_parser.dest(), ' binary: ', code_builder.dest(file_parser.dest()))
+            print('Comp: ', file_parser.comp(), ' binary: ', code_builder.comp(file_parser.comp()))
+            print('Jump: ', file_parser.jump(), ' binary: ', code_builder.jump(file_parser.jump()))
         print('')
         keep_going = file_parser.hasMoreLines()
 
